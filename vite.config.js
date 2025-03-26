44d4c346156bc308b0d288d0b8c6d8000d6cdda1
Shreykumar <shreykumarshrey@gmail.com>
2025-03-26 16:13:56 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: 'cookingrecipie'
   plugins: [
     tailwindcss(),
+    react(),
   ],
 })
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   base: '/<repo-name>/', // <-- Add this line (replace <repo-name> with your actual repository name)
+//   plugins: [react()],
+// });
